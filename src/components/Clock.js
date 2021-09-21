c9c93276591d726c4f57df4fab0d0b2559109611
maksudul.it <maksudul.it@nationalbankbd.com>
2021-09-21 18:22:04 +0600

    just leaving office :p

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,19 +2,29 @@ import React from "react";
 
 class Clock extends React.Component{
 
-    constructor(props){
-        super(props);
-        this.state={
-            date: new Date()
-        };
-    }
+    // constructor and its parameter as prop is used if we need the props to initiate a value to state
+    // constructor(props){
+    //     super(props);
+    //     this.state={
+    //         date: new Date()
+    //     };
+    // }
+    //as we dont need props to initate any value to state so we simply follow below process
+
+    state = {date: new Date()}
 
     componentDidMount(){
-        setInterval(() => {
-            this.setState({
-                date: new Date(),
-            });
+       this.componentInterval =  setInterval(() => {
+            this.tick()
         }, 1000);
+    }
+    tick(){
+        this.setState({
+            date: new Date(),
+        });
+    }
+    componentWillUnmount(){
+        clearInterval(this.componentInterval);
     }
     
 
