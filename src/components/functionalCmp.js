42cd071aec006afe785b72a0dee9277e389a7c6b
maksudul.it <maksudul.it@nationalbankbd.com>
2021-09-21 12:17:26 +0600

    useEffect is added

--- src/components/functionalCmp.js
+++ src/components/functionalCmp.js
@@ -1,13 +1,16 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 
 // this way is called  "without destructuring"
 const FuncComp = (props)=>{
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState('a');
+    useEffect(()=>{
+        document.title = `${count}`;
+    });
 
     return(
         <div>
            <p> Hi. I'm a functional Component!! my name is <b>{props.name}</b></p>
-           <button onClick={()=>{setCount(count+1);}}>
+           <button onClick={()=>{setCount(count+'a');}}>
                {count}
            </button>
         </div>
